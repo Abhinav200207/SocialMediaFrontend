dd036d0bbc7058ecbb41651815e1a6921ec77439
Abhinav200207 <82315896+Abhinav200207@users.noreply.github.com>
2022-05-19 16:14:29 +0530

    Update Construction.jsx

--- src/Components/Construction/Construction.jsx
+++ src/Components/Construction/Construction.jsx
@@ -11,7 +11,7 @@ const Construction = () => {
                     {/* <Typography variant="h3" style={{ padding: "2vmax" }}>Website</Typography> */}
                     <Typography variant="h3" style={{ padding: "2vmax" }}>Website Under Construction</Typography>
                     <img alt="Logo" src={cons}/>
-                    <Typography variant="h6" style={{ padding: "2vmax" }}>Frontend Github Repository: <a href="https://github.com/">Link</a></Typography>
+                    <Typography variant="h6" style={{ padding: "2vmax" }}>Frontend Github Repository: <a href="https://github.com/Abhinav200207/SocialMediaFrontend">Link</a></Typography>
                     <Typography variant="h6" style={{ padding: "2vmax" }}>Backend Github Repository: <a href="https://github.com/Abhinav200207/Social-Media-Web-App">Link</a></Typography>
                 </div>
             </div>
